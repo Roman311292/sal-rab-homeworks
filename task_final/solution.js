185f12a44bd1630f604bfc4caa926a9bac82a14b
Roman Shishkin <Mr.fractal88@gmail.com>
2022-06-27 21:48:48 +0300

    LAST1

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,7 +39,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push = {tittle: [goods[i].tittle], count: [goods[i].count]};
+        data.goods.push = {title: [goods[i].title], count: [goods[i].count]};
     }
 
     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
